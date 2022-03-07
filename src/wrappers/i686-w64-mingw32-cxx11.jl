e8bb49bb8388c3571aef71f6c40ec3a19685603b
jlbuild <juliabuildbot@gmail.com>
2022-03-07 23:28:06 +0000

    protoc_jll build 3.13.0+0

--- src/wrappers/i686-w64-mingw32-cxx11.jl
+++ src/wrappers/i686-w64-mingw32-cxx11.jl
@@ -2,13 +2,13 @@
 export libprotoc, protoc
 
 JLLWrappers.@generate_wrapper_header("protoc")
-JLLWrappers.@declare_library_product(libprotoc, "libprotoc-30.dll")
+JLLWrappers.@declare_library_product(libprotoc, "libprotoc-24.dll")
 JLLWrappers.@declare_executable_product(protoc)
 function __init__()
     JLLWrappers.@generate_init_header()
     JLLWrappers.@init_library_product(
         libprotoc,
-        "bin\\libprotoc-30.dll",
+        "bin\\libprotoc-24.dll",
         RTLD_LAZY | RTLD_DEEPBIND,
     )
 
